126f66e171f50a62d280b5a0508f70ad3984d3f4
robertpanvip <robertpanvip@163.com>
2024-12-20 11:12:07 +0800

    Update HTMLComment.tsx

--- src/SingleObserver/HTMLComment.tsx
+++ src/SingleObserver/HTMLComment.tsx
@@ -9,36 +9,59 @@ type ElementLike = {
   style: object;
 };
 
-const createElement = document.createElement;
 const TagSymbol = `comment__`;
 
-//react 内部是用这个创建节点的 由于react本身不支持创建注释节点 这里hack一下
-document.createElement = function (
-  tagName: string,
-  options?: ElementCreationOptions
-) {
-  if (
-    tagName === "noscript" &&
-    options?.is &&
-    options.is.startsWith(TagSymbol)
-  ) {
-    const regex = new RegExp(`^${TagSymbol}(.*)$`);
-    const match = options?.is.match(regex);
-    if (match) {
-      const data = match[1].trim?.();
-      const comment = document.createComment(data) as unknown as ElementLike;
-      comment.setAttribute = () => true;
-      comment.style = {};
-      return comment as unknown as HTMLElement;
-    }
-  }
-  return createElement.call(this, tagName, options);
+const canUseDom = () => {
+  return !!(
+    typeof window !== "undefined" &&
+    window.document &&
+    window.document.createElement
+  );
 };
+
+function intercept() {
+  const createElement = document.createElement;
+
+  const reset = () => {
+    document.createElement = createElement;
+  };
+
+  //react 内部是用这个创建文本节点的 由于react本身不支持创建注释节点 这里hack一下
+  document.createElement = function (
+    tagName: string,
+    options?: ElementCreationOptions
+  ) {
+    if (
+      tagName === "noscript" &&
+      options?.is &&
+      options.is.startsWith(TagSymbol)
+    ) {
+      const regex = new RegExp(`^${TagSymbol}(.*)$`);
+      const match = options?.is.match(regex);
+      if (match) {
+        const data = match[1].trim?.();
+        const comment = document.createComment(data) as unknown as ElementLike;
+        comment.setAttribute = () => true;
+        comment.style = {};
+        reset();
+        return comment as unknown as HTMLElement;
+      }
+    }
+    return createElement.call(this, tagName, options);
+  };
+}
 
 function CommentRender(
   { data = "" }: CommentProps,
   ref: React.ForwardedRef<null | Comment>
 ) {
+  const mounted = React.useRef(false);
+  if (!mounted.current) {
+    if (canUseDom()) {
+      intercept();
+    }
+    mounted.current = true;
+  }
   return (
     <noscript
       ref={ref as React.ForwardedRef<null | HTMLMetaElement>}
