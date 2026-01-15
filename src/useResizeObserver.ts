9cf43442a011ee501a06f520103854fe8c70b586
二货机器人 <smith3816@gmail.com>
2026-01-15 11:25:44 +0800

    chore: init

---
+++ src/useResizeObserver.ts
@@ -0,0 +1,8 @@
+import type { OnResize } from '.';
+
+// TODO: Ref observerUtil.ts, realize this.
+export default function useResizeObserver(
+  enabled: boolean,
+  getTarget: () => HTMLElement,
+  onResize: OnResize,
+) {}
